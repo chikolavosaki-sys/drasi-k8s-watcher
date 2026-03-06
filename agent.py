ed0ebf88e3f7237eedb48af305582da0c3b77c9d
Prithvi Chandra Surya <chikolavosaki@gmail.com>
2026-03-06 22:12:43 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -4,11 +4,11 @@ from dapr.ext.fastapi import DaprApp
 app = FastAPI()
 dapr_app = DaprApp(app)
 
-# This is the routing magic you will eventually wrap into a custom SDK decorator!
+# This is the routing  wrap into a custom SDK decorator!
 @dapr_app.subscribe(pubsub='drasi-pubsub', topic='drasi-events')
 def wake_up_agent(event: dict):
     print("--------------------------------------------------", flush=True)
-    print("🤖 I AM AWAKE! Drasi detected a change in the K8s cluster!", flush=True)
+    print(" AWAKE! Drasi detected a change in the K8s cluster!", flush=True)
     print(f"Event Data: {event}", flush=True)
     print("--------------------------------------------------", flush=True)
     return {"status": "SUCCESS"}
